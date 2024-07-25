3ba11c407a7b5fc2b99c8a1b7ff15fe9a99d83f2
kucing kalem <frosi551@gmail.com>
2024-07-25 21:14:59 +0700

    fix ui laptop

--- src/components/home/carousel.js
+++ src/components/home/carousel.js
@@ -5,7 +5,7 @@ import Btn from "./button"
 const Carousel = () => {
   return (
     <section className={`${styles.container}container p-10 bg-[#677D6A]`}>
-      <div className="flex justify-end pl-10 pr-10">
+      <div className="flex justify-end pl-10 mb-5">
           <Btn />
       </div>
         <Myswiper/>
